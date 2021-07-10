d33e962664d30ef224b35b74309b93e4dc481587
ArianeDucellier <ariane.ducellier.pro@gmail.com>
2021-07-10 16:49:43 -0700

    Plot tidal stress

--- src/tidal_stress.py
+++ src/tidal_stress.py
@@ -17,9 +17,6 @@ pylab.rcParams.update(params)
 # Number of boxes for plotting
 nbox = 20
 
-# Tidal stress file
-data = loadmat('../data/For_Ken_California_LFE/LFEs2.mat')
-
 # List of LFE families
 templates = np.loadtxt('../data/Plourde_2015/templates_list.txt', \
     dtype={'names': ('name', 'family', 'lat', 'lon', 'depth', 'eH', \
@@ -39,15 +36,13 @@ for i in range(0, np.shape(templates)[0]):
     # Look only at good data
     if threshold['threshold_perm'].iloc[i] > 0.0:
 
-        # Look for corresponding location of tidal stress
-        latitude = templates[i][2]
-        longitude = templates[i][3]
-        for k in range(0, len(data['LFE']['TempLabel'][0][0])):
-            if ((data['LFE']['lat'][0][0][k] == latitude) & \
-                (data['LFE']['lon'][0][0][k] == longitude)):
-                namefile = data['LFE']['TempLabel'][0][0][k][0][0]
-                j = k
-        print(templates[i][0].astype(str), namefile, i, j)
+        # Load data
+        filename = '../data/tidal_stress/' + templates[i][0].astype(str) + '.pkl'
+        tidal_stress = pickle.load(open(filename, 'rb'))
+        time = tidal_stress[0]
+        norm = tidal_stress[1]
+        shear = tidal_stress[2]
+        vol = tidal_stress[3]
 
         # Open LFE catalog
         namedir = '../data/Ducellier/catalogs/' + templates[i][0].astype(str)
@@ -70,72 +65,97 @@ for i in range(0, np.shape(templates)[0]):
         df['date'] = date
 
         # Look for value of tidal stress at the time of each LFE 
-        shear = np.repeat(np.nan, len(df))
-        norm = np.repeat(np.nan, len(df))
-        vol = np.repeat(np.nan, len(df))
+        shear_LFE = np.repeat(np.nan, len(df))
+        norm_LFE = np.repeat(np.nan, len(df))
+        vol_LFE = np.repeat(np.nan, len(df))
         for k in range(len(df)):
-            indices = np.where(24 * 4 * np.abs(data['LFE']['tvec'][0][0][0, j] - df.date.loc[k]) <= 1)[1]
+            indices = np.where(24 * np.abs(time - df.date.loc[k]) <= 1)[1]
             if len(indices) > 0:
-                shear[k] = np.mean(data['LFE']['shear'][0][0][0, j][0, indices])
-                norm[k] = np.mean(data['LFE']['norm'][0][0][0, j][0, indices])
-                vol[k] = np.mean(data['LFE']['vol'][0][0][0, j][0, indices])
-        df['shear'] = shear
-        df['norm'] = norm
-        df['vol'] = vol
+                shear_LFE[k] = np.interp(df.date.loc[k], time[0, indices], shear[indices])
+                norm_LFE[k] = np.interp(df.date.loc[k], time[0, indices], norm[indices])
+                vol_LFE[k] = np.interp(df.date.loc[k], time[0, indices], vol[indices])
+        df['shear'] = shear_LFE
+        df['norm'] = norm_LFE
+        df['vol'] = vol_LFE
 
         # Plot
-        plt.figure(1, figsize=(10, 18))
+        plt.figure(1, figsize=(20, 18))
 
         # Shear
-        min_stress = np.min(data['LFE']['shear'][0][0][0, j])
-        max_stress = np.max(data['LFE']['shear'][0][0][0, j])
+        min_stress = np.min(shear)
+        max_stress = np.max(shear)
         boxes = np.linspace(min_stress, max_stress, nbox + 1)
+        size_box = (max_stress - min_stress) / (4 * nbox)
 
         count = np.zeros(nbox)
         count_shear = np.zeros(nbox)
         for k in range(0, nbox):
             count[k] = len(df.loc[(df.shear >= boxes[k]) & (df.shear <= boxes[k + 1])])
-            count_shear[k] = np.sum((data['LFE']['shear'][0][0][0, j] >= boxes[k]) & (data['LFE']['shear'][0][0][0, j] <= boxes[k + 1]))
-        print(count)
-        print(count_shear)
-        norm = np.sum(count) / np.sum(count / count_shear)
+            count_shear[k] = np.sum((shear >= boxes[k]) & (shear <= boxes[k + 1]))
+        normalize = np.sum(count) / np.sum(count / count_shear)
 
-        ax1 = plt.subplot(311)
-        plt.bar(0.5 * (boxes[1:] + boxes[:-1]), norm * count / count_shear, 0.05)
+        ax1 = plt.subplot(321)
+        expected = len(df) * count_shear / np.sum(count_shear)
+        plt.plot(0.5 * (boxes[1:] + boxes[:-1]), expected, color='black')
+        plt.bar(0.5 * (boxes[1:] + boxes[:-1]), count, size_box)
+        plt.xlabel('Shear stress (kPa)', fontsize=24)
+        plt.ylabel('Number of LFEs', fontsize=24)
+
+        ax2 = plt.subplot(322)
+        plt.plot(0.5 * (boxes[1:] + boxes[:-1]), normalize * expected / count_shear, color='black')
+        plt.bar(0.5 * (boxes[1:] + boxes[:-1]), normalize * count / count_shear, size_box)
         plt.xlabel('Shear stress (kPa)', fontsize=24)
         plt.ylabel('Number of LFEs', fontsize=24)
 
         # Normal
-        min_stress = np.min(data['LFE']['norm'][0][0][0, j])
-        max_stress = np.max(data['LFE']['norm'][0][0][0, j])
+        min_stress = np.min(norm)
+        max_stress = np.max(norm)
         boxes = np.linspace(min_stress, max_stress, nbox + 1)
+        size_box = (max_stress - min_stress) / (4 * nbox)
 
         count = np.zeros(nbox)
         count_norm = np.zeros(nbox)
         for k in range(0, nbox):
             count[k] = len(df.loc[(df.norm >= boxes[k]) & (df.norm <= boxes[k + 1])])
-            count_norm[k] = np.sum((data['LFE']['norm'][0][0][0, j] >= boxes[k]) & (data['LFE']['norm'][0][0][0, j] <= boxes[k + 1]))
-        norm = np.sum(count) / np.sum(count / count_norm)
+            count_norm[k] = np.sum((norm >= boxes[k]) & (norm <= boxes[k + 1]))
+        normalize = np.sum(count) / np.sum(count / count_norm)
 
-        ax2 = plt.subplot(312)
-        plt.bar(0.5 * (boxes[1:] + boxes[:-1]), norm * count / count_norm, 0.05)
+        ax3 = plt.subplot(323)
+        expected = len(df) * count_norm / np.sum(count_norm)
+        plt.plot(0.5 * (boxes[1:] + boxes[:-1]), expected, color='black')
+        plt.bar(0.5 * (boxes[1:] + boxes[:-1]), count, size_box)
+        plt.xlabel('Normal stress (kPa)', fontsize=24)
+        plt.ylabel('Number of LFEs', fontsize=24)
+
+        ax4 = plt.subplot(324)
+        plt.plot(0.5 * (boxes[1:] + boxes[:-1]), normalize * expected / count_norm, color='black')
+        plt.bar(0.5 * (boxes[1:] + boxes[:-1]), normalize * count / count_norm, size_box)
         plt.xlabel('Normal stress (kPa)', fontsize=24)
         plt.ylabel('Number of LFEs', fontsize=24)
 
         # Volume
-        min_stress = np.min(data['LFE']['vol'][0][0][0, j])
-        max_stress = np.max(data['LFE']['vol'][0][0][0, j])
+        min_stress = np.min(vol)
+        max_stress = np.max(vol)
         boxes = np.linspace(min_stress, max_stress, nbox + 1)
+        size_box = (max_stress - min_stress) / (4 * nbox)
 
         count = np.zeros(nbox)
         count_vol = np.zeros(nbox)
         for k in range(0, nbox):
             count[k] = len(df.loc[(df.vol >= boxes[k]) & (df.vol <= boxes[k + 1])])
-            count_vol[k] = np.sum((data['LFE']['vol'][0][0][0, j] >= boxes[k]) & (data['LFE']['vol'][0][0][0, j] <= boxes[k + 1]))
-        norm = np.sum(count) / np.sum(count / count_vol)
+            count_vol[k] = np.sum((vol >= boxes[k]) & (vol <= boxes[k + 1]))
+        normalize = np.sum(count) / np.sum(count / count_vol)
 
-        ax3 = plt.subplot(313)
-        plt.bar(0.5 * (boxes[1:] + boxes[:-1]), norm * count / count_vol, 0.05)
+        ax5 = plt.subplot(325)
+        expected = len(df) * count_vol / np.sum(count_vol)
+        plt.plot(0.5 * (boxes[1:] + boxes[:-1]), expected, color='black')
+        plt.bar(0.5 * (boxes[1:] + boxes[:-1]), count, size_box)
+        plt.xlabel('Volume stress (kPa)', fontsize=24)
+        plt.ylabel('Number of LFEs', fontsize=24)
+
+        ax6 = plt.subplot(326)
+        plt.plot(0.5 * (boxes[1:] + boxes[:-1]), normalize * expected / count_vol, color='black')
+        plt.bar(0.5 * (boxes[1:] + boxes[:-1]), normalize * count / count_vol, size_box)
         plt.xlabel('Volume stress (kPa)', fontsize=24)
         plt.ylabel('Number of LFEs', fontsize=24)
 
@@ -144,4 +164,7 @@ for i in range(0, np.shape(templates)[0]):
         ax1.clear()
         ax2.clear()
         ax3.clear()
+        ax4.clear()
+        ax5.clear()
+        ax6.clear()
         plt.close(1)
